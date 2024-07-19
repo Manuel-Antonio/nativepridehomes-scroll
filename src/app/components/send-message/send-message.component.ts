ab58908b115adeed1b27e60dc4b9c86c2d9604f9
Manuel Huaman <manuelhuaman1998@gmail.com>
2024-07-19 13:57:10 -0500

    Algunas mejoras

--- src/app/components/send-message/send-message.component.ts
+++ src/app/components/send-message/send-message.component.ts
@@ -4,19 +4,22 @@ import { WhatsappService } from 'src/app/services/whatsapp.service';
 @Component({
   selector: 'app-send-message',
   templateUrl: './send-message.component.html',
-  styleUrls: ['./send-message.component.css']
+  styleUrls: ['./send-message.component.css'],
 })
 export class SendMessageComponent {
   message: string = '';
   isWhatsappOpen = false;
-  
-  constructor(
-    private whatsappService: WhatsappService
-  ) {}
+
+  constructor(private whatsappService: WhatsappService) {}
 
   sendWhatsappMessage() {
+    if (this.message.trim() == '') {
+      this.message =
+        'Hello! I am interested in learning more about the services you offer. I am looking to build a house and would like to know more about your process, the materials you use and approximate costs. Could you provide me with more information? Thank you.';
+    }
     this.whatsappService.sendMessage(this.message);
   }
+
   toggleWhatsapp() {
     this.isWhatsappOpen = !this.isWhatsappOpen;
   }
