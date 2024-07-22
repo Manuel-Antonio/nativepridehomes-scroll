a49283fc8c0bfcf54c8dadfcdfb8dd1d333b02bd
Manuel Huaman <manuelhuaman1998@gmail.com>
2024-07-21 22:14:03 -0500

    Cambios en html, our team de about eliminado y cambios de img de testimonials

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { profiles } from 'src/app/data/profiles.data';
-import { Profile } from 'src/app/models/profile.model';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  profiles : Profile[] = profiles
+  
   ngOnInit() {
   }
 }
--- src/app/data/testimonials.data.ts
+++ src/app/data/testimonials.data.ts
@@ -2,28 +2,28 @@ import { Testimonial } from '../models/testimonial.model';
 
 export const testimonials: Testimonial[] = [
   new Testimonial(
-    'Emily Johnson',
-    'The company did an exceptional job on our house. Highly recommended!',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1720740267/NativePrideHomes/testimonials/perfil1_neee4e.png',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1721506643/NativePrideHomes/testimonials-webp/perfil1_neee4e_2_11zon_e57bdm.webp',
-    'Detailed and precise work on the new extension of our house.',
-    'June 1, 2023',
-    5
-  ),
-  new Testimonial(
     'Michael Smith',
     'Professional and efficient. The result was impressive.',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1720740268/NativePrideHomes/testimonials/perfil2_gyebpu.png',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1721506644/NativePrideHomes/testimonials-webp/perfil2_gyebpu_3_11zon_njsbiu.webp',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617714/NativePrideHomes/testimonials/22_2_11zon_1_11zon_ekuv4r.jpg',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617389/NativePrideHomes/testimonials-webp/22_2_11zon_csb1a0.webp',
     'Efficient completion of our office renovation.',
     'July 12, 2023',
     4
   ),
   new Testimonial(
+    'Emily Johnson',
+    'The company did an exceptional job on our house. Highly recommended!',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617721/NativePrideHomes/testimonials/1223_1_11zon_10_11zon_hp5aff.jpg',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617393/NativePrideHomes/testimonials-webp/1223_1_11zon_cl9vzm.webp',
+    'Detailed and precise work on the new extension of our house.',
+    'June 1, 2023',
+    5
+  ),
+  new Testimonial(
     'Sarah Williams',
     "Outstanding quality and attention to detail. Couldn't be happier.",
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1720740269/NativePrideHomes/testimonials/perfil3_bdoct0.png',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1721506644/NativePrideHomes/testimonials-webp/perfil3_bdoct0_4_11zon_byxnrq.webp',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617713/NativePrideHomes/testimonials/84_3_11zon_2_11zon_lr9j32.jpg',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617389/NativePrideHomes/testimonials-webp/84_3_11zon_dhsjt1.webp',
     'Excellent craftsmanship on our new kitchen.',
     'August 5, 2023',
     5
@@ -31,26 +31,17 @@ export const testimonials: Testimonial[] = [
   new Testimonial(
     'David Brown',
     'Excellent service and workmanship. The team exceeded our expectations.',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1720740266/NativePrideHomes/testimonials/perfil4_os3lbs.png',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1721506644/NativePrideHomes/testimonials-webp/perfil4_os3lbs_5_11zon_jqfgs4.webp',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617715/NativePrideHomes/testimonials/87_4_11zon_3_11zon_dmmx0k.jpg',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617389/NativePrideHomes/testimonials-webp/87_4_11zon_dsp6ov.webp',
     'Amazing job on our backyard landscaping.',
     'September 10, 2023',
     4
   ),
   new Testimonial(
-    'Jessica Taylor',
-    'From start to finish, the process was seamless and stress-free.',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1720740265/NativePrideHomes/testimonials/perfil5_kvfwmq.png',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1721506645/NativePrideHomes/testimonials-webp/perfil5_kvfwmq_6_11zon_cehwvs.webp',
-    'Smooth and hassle-free renovation of our living room.',
-    'October 20, 2023',
-    5
-  ),
-  new Testimonial(
     'John Davis',
     'Their commitment to quality is evident in every aspect of the project.',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1720740266/NativePrideHomes/testimonials/perfil6_lpddcx.png',
-    'https://res.cloudinary.com/djsl4a5py/image/upload/w_72,h_72,c_fill/v1721506646/NativePrideHomes/testimonials-webp/perfil6_lpddcx_1_11zon_sviogr.webp',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617716/NativePrideHomes/testimonials/200_6_11zon_5_11zon_lelzc2.jpg',
+    'https://res.cloudinary.com/djsl4a5py/image/upload/v1721617389/NativePrideHomes/testimonials-webp/200_6_11zon_g5hsdn.webp',
     'High-quality construction of our new garage.',
     'November 15, 2023',
     5
